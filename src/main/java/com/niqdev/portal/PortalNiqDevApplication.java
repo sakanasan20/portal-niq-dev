ec25a0eb600e87adc7fa33d3cac2af72a9bb1172
Nick Chen <sakanasan20@gmail.com>
2025-08-05 22:31:50 +0800

    Initial commit

---
+++ src/main/java/com/niqdev/portal/PortalNiqDevApplication.java
@@ -0,0 +1,13 @@
+package com.niqdev.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalNiqDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalNiqDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niqdev/portal/PortalNiqDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niqdev.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalNiqDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
